975b259c57c2e278f95344a9326667a803bc1ef4
JaneBee24 <134943735+JaneBee24@users.noreply.github.com>
2025-03-02 05:31:12 +0000

    Deploying to gh-pages from @ JaneBee24/goit-js-hw-11@e00b0324e1a8ffc0e9a6b3ad3416d2947cf2d310 🚀

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-/* empty css                      */import{a as p,S as f}from"./assets/vendor-DTuICwlt.js";import a from"izitoast";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const m="49034890-15d0e202b9bb59c7b310d7a4f",d="https://pixabay.com/api/";async function y(i,t=1,o=40){try{const s=await p.get(d,{params:{key:m,q:i,image_type:"photo",orientation:"horizontal",safesearch:!0,page:t,per_page:o}});return s.data.hits.length===0&&a.warning({title:"Внимание",message:"По вашему запросу ничего не найдено!",position:"topRight"}),s.data}catch(s){throw a.error({title:"Ошибка",message:"Не удалось загрузить изображения. Попробуйте позже!",position:"topRight"}),console.error("Ошибка при загрузке изображений:",s),s}}function h(i){const t=document.querySelector(".gallery"),o=i.map(({webformatURL:e,largeImageURL:r,tags:n,likes:l,views:c,comments:u,downloads:g})=>`
-      <li class="gallery-item">
-        <a class="gallery-link" href="${r}">
-          <img class="gallery-image" src="${e}" alt="${n}" loading="lazy">
-        </a>
-        <div class="image-info">
-          <p><strong>Likes:</strong> ${l}</p>
-          <p><strong>Views:</strong> ${c}</p>
-          <p><strong>Comments:</strong> ${u}</p>
-          <p><strong>Downloads:</strong> ${g}</p>
-        </div>
-      </li>`).join("");t.innerHTML=o,new f(".gallery a").refresh()}const b=document.querySelector("#search-form"),L=document.querySelector(".gallery");b.addEventListener("submit",async i=>{i.preventDefault();const t=i.target.elements.searchQuery.value.trim();if(!t){alert("Введите поисковый запрос!");return}L.innerHTML="";try{const o=await y(t);if(o.hits.length===0){alert("Картинки не найдены. Попробуйте другой запрос.");return}h(o.hits)}catch(o){console.error("Ошибка загрузки изображений:",o)}});
+/* empty css                      */import{a as c,S as u}from"./assets/vendor-DTuICwlt.js";import"izitoast";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const p="49034890-15d0e202b9bb59c7b310d7a4f",d="https://pixabay.com/api/";async function f(a,t=1,o=40){try{const s=await c.get(d,{params:{key:p,q:a,image_type:"photo",orientation:"horizontal",safesearch:!0,page:t,per_page:o}});return{images:s.data.hits,total:s.data.totalHits}}catch(s){throw console.error("Ошибка при получении данных:",s),new Error("Не удалось загрузить изображения")}}const m=document.querySelector(".gallery");let g=new u(".gallery a",{captionsData:"alt",captionDelay:250});function y(a){const t=a.map(({webformatURL:o,largeImageURL:s,tags:e,likes:r,views:n,comments:i,downloads:l})=>`
+        <li class="gallery-item">
+            <a class="gallery-link" href="${s}">
+                <img class="gallery-image" src="${o}" alt="${e}" loading="lazy" />
+            </a>
+            <div class="image-info">
+                <p><strong>Likes:</strong> ${r}</p>
+                <p><strong>Views:</strong> ${n}</p>
+                <p><strong>Comments:</strong> ${i}</p>
+                <p><strong>Downloads:</strong> ${l}</p>
+            </div>
+        </li>
+    `).join("");m.insertAdjacentHTML("beforeend",t),g.refresh()}const h=document.querySelector("#search-form"),b=document.querySelector(".gallery");h.addEventListener("submit",async a=>{a.preventDefault();const t=a.target.elements.searchQuery.value.trim();if(!t){alert("Введите поисковый запрос!");return}b.innerHTML="";try{const o=await f(t);if(o.hits.length===0){alert("Картинки не найдены. Попробуйте другой запрос.");return}y(o.hits)}catch(o){console.error("Ошибка загрузки изображений:",o)}});
 //# sourceMappingURL=index.js.map
